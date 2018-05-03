35b7255a5f18955326eb7afbd340bda0e284fa8e
Aayog Koirala <1aayogkoirala@gmail.com>
2018-05-03 21:39:48 +0545

    The Matrix Screen effect using Nepali.
    Devanagiri unicode character to make the Matrix raining text effect.

--- sketch.js
+++ sketch.js
@@ -1,22 +1,64 @@
-function setup() {
-  // put setup code here
-    createCanvas(600,600);
-}
-function draw() {
-  // put drawing code here
-    background(150);
-    translate(width/2,height/2);
-    drawRect();
-    frameRate(2);
 
+var symbolSize = 20;
+var streams = [];
+
+function setup(){
+    createCanvas(window.innerWidth, window.innerHeight);
+    textSize(symbolSize);
+    var x = 0;
+    for ( var i = 0; i < width / symbolSize; i++ ){
+        var stream = new Stream();
+        stream.generateSymbols(x,random(-500,0));
+        streams.push(stream);
+        x += symbolSize;
+    }
 }
-function drawRect(){
-    var angle= random(0,360);
-    for(var i =1; i < 600; i += 1){
-        if(i % 20 == 0){
-            fill(random(0,255),random(0,255),random(0,255),random(0,150));
+
+function draw(){
+    background(0,150);
+    streams.forEach(function (stream) {
+        stream.render();
+    })
+}
+
+function Symbol(x, y,speed) {
+    this.x = x;
+    this.y = y;
+    this.value;
+    this.speed = speed;
+    this.interval = round(random(2,40));
+
+    this.setToRandomSymbol = function() {
+        if(frameCount % this.interval == 0){
+            this.value = String.fromCharCode(
+                0x0900 + round(random(0, 128))
+            );
         }
-        rect(i,i,80,70);
-        rotate(angle);
+    }
+    this.rain = function(){
+        this.y = (this.y >= height)? 0 : this.y += this.speed;
+    }
+}
+
+function Stream() {
+    this.symbols = [];
+    this.totalSymbols = round(random(5,30));
+    this.speed = random(1,15);
+    this.generateSymbols = function(x,y) {
+        for (var i = 0; i <= this.totalSymbols; i++){
+            symbol = new Symbol(x,y,this.speed);
+            symbol.setToRandomSymbol();
+            this.symbols.push(symbol);
+            y -= symbolSize+2;
+        }
+    }
+
+    this.render = function() {
+        this.symbols.forEach( function(symbol) {
+            fill(0, 255, 70);
+            text(symbol.value, symbol.x, symbol.y);
+            symbol.rain();
+            symbol.setToRandomSymbol();
+        });
     }
 }
